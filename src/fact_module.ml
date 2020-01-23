4bee1298323e24a6ed20b109c6ed020c0ed0e835
Takashi Masuyama <mamewotoko@gmail.com>
2020-01-24 08:14:07 +0900

    modify to exit normally

--- src/fact_module.ml
+++ src/fact_module.ml
@@ -2,4 +2,5 @@ let rec fact n = if n = 1 then 1 else n * fact (n - 1)
 
 (* inline test using ppx_inline_test *)
 let%test _ = fact 5 = 120
-let%test _ = fact 5 = 0
+(* following test should fail *)
+(* let%test _ = fact 5 = 0 *)
